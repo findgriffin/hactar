9e78969362bab79640320b8fd6c38ea81de7d9f2
David Griffin <dgriffin@toshiba-tap.com>
2013-09-19 14:05:48 +1000

    Update testweb:  * rename webTestCase to TestWeb  * replace assert statements with unittest assertIn etc.  * check status code /add requests  * rename test_messages to test_add_nugget  * add test_add_nuggets  * add test_dup_nuggets

--- test/testweb.py
+++ test/testweb.py
@@ -13,7 +13,7 @@ import unittest
 import tempfile
 
 
-class webTestCase(unittest.TestCase):
+class TestWeb(unittest.TestCase):
 
     def setUp(self):
         """Before each test, set up a blank database"""
@@ -47,28 +47,65 @@ class webTestCase(unittest.TestCase):
         """Make sure login and logout works"""
         rv = self.login(web.app.config['USERNAME'],
                         web.app.config['PASSWORD'])
-        assert b'You were logged in' in rv.data
+        self.assertIn(b'You were logged in', rv.data)
         rv = self.logout()
-        assert b'You were logged out' in rv.data
+        self.assertIn(b'You were logged out', rv.data)
         rv = self.login(web.app.config['USERNAME'] + 'x',
                         web.app.config['PASSWORD'])
-        assert b'Invalid username' in rv.data
+        self.assertIn(b'Invalid username', rv.data)
         rv = self.login(web.app.config['USERNAME'],
                         web.app.config['PASSWORD'] + 'x')
-        assert b'Invalid password' in rv.data
+        self.assertIn(b'Invalid password', rv.data)
 
-    def test_messages(self):
-        """Test that messages work"""
+    def test_add_nugget(self):
+        """Test adding a nugget with flask"""
         self.login(web.app.config['USERNAME'],
                    web.app.config['PASSWORD'])
-        rv = self.app.post('/add', data=dict(
-            title='<Hello>',
-            text='<strong>HTML</strong> allowed here'
+        uri = 'http://foobar.com'
+        desc = 'a description of foobar'
+        rv = self.app.post('/add', data=dict( uri=uri, desc=desc,
         ), follow_redirects=True)
-        self.assertNotIn('No nuggets here so far', rv.data)
-        self.assertNotIn('&lt;Hello&gt;', rv.data)
-        self.assertNotIn('<strong>HTML</strong> allowed here', rv.data)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn('<li><h2>%s</h2>%s' % (uri, desc), rv.data)
 
+    def test_add_nuggets(self):
+        """Test adding some nuggets with flask"""
+        self.login(web.app.config['USERNAME'],
+                   web.app.config['PASSWORD'])
+        uri0 = 'http://foobar.com'
+        desc0 = 'a description of foobar'
+        uri1 = 'http://foobar.com/stuff'
+        desc1 = 'a description of foobar/stuff'
+        uri2 = 'http://foobar.com/stuff/more'
+        desc2 = 'a description of foobar/stuff/more'
+        rv0 = self.app.post('/add', data=dict( uri=uri0, desc=desc0,
+        ), follow_redirects=True)
+        rv1 = self.app.post('/add', data=dict( uri=uri1, desc=desc1,
+        ), follow_redirects=True)
+        rv2 = self.app.post('/add', data=dict( uri=uri2, desc=desc2,
+        ), follow_redirects=True)
+        self.assertEqual(rv1.status_code, 200)
+        self.assertIn('<li><h2>%s</h2>%s' % (uri0, desc0), rv2.data)
+        self.assertIn('<li><h2>%s</h2>%s' % (uri1, desc1), rv2.data)
+        self.assertIn('<li><h2>%s</h2>%s' % (uri2, desc2), rv2.data)
+
+    def test_dup_nuggets(self):
+        """Test attempting to add duplicate nuggets"""
+        self.login(web.app.config['USERNAME'],
+                   web.app.config['PASSWORD'])
+        uri0 = 'http://foobar.com'
+        desc0 = 'a description of foobar'
+        uri1 = uri0
+        desc1 = 'a description of foobar/stuff'
+        rv0 = self.app.post('/add', data=dict( uri=uri0, desc=desc0,
+        ), follow_redirects=True)
+        rv1 = self.app.post('/add', data=dict( uri=uri1, desc=desc1,
+        ), follow_redirects=True)
+        self.assertEqual(rv1.status_code, 200)
+        errstr = 'Nugget with that URI or description already exists'
+        self.assertIn('<div class=flash>%s</div>' % errstr, rv1.data)
+        self.assertIn('<li><h2>%s</h2>%s' % (uri0, desc0), rv1.data)
+        self.assertNotIn('<li><h2>%s</h2>%s' % (uri1, desc1), rv1.data)
 
 if __name__ == '__main__':
     unittest.main()
