1bdba3acbb8d98406a0deaa7ff3c250c9e568f99
David Griffin <dgriffin@toshiba-tap.com>
2013-09-26 16:13:06 +1000

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,32 @@
 """ Fabfile for hactar project."""
-import cuisine
 import os
+import re
+
+import cuisine
 from fabric.api import cd, env
 
 env.hosts = ['auroraborealis.com.au']
 LOCATION = 'hactar'
 GIT = 'https://github.com/findgriffin/hactar.git'
-PASS = 'string that shows we passed the tests'
+MIN_TESTS = 13
+MAX_SKIP = 5
+
+def passed(output):
+    ran = re.findall('Ran \d+ tests', output)
+    assert len(ran) == 1
+    tests = int(ran[0].split()[1])
+    skip = re.findall('OK \(SKIP=\d+\)', output)
+    assert len(skip) <= 1
+    if skip:
+        skipped = int(skip[0].split('=')[-1].rstrip(')'))
+    else:
+        if not output.endswith('OK'):
+            return False
+        skipped = 0
+    if skipped > MAX_SKIP or tests < MIN_TESTS:
+        return False
+    return True
+
 
 def setup_host():
     """ Setup a host to the point where it can run hactar."""
@@ -27,7 +47,7 @@ def update_hactar():
     with cd(LOCATION):
         cuisine.run('git pull')
         cuisine.run('git clean')
-        if PASS in cuisine.run('nosetests'):
+        if passed(cuisine.run('nosetests')):
             # complete rollout
             pass
         else:
