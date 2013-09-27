1e39780530b101beea62783e94b921b6b4913bb9
David Griffin <findgriffin@gmail.com>
2013-09-28 06:02:32 +1000

    mostly implement the content retrievel and storage of page scraping

--- hactar/scraper.py
+++ hactar/scraper.py
@@ -1,8 +1,19 @@
 import re
+import json
+import datetime
 
 from requests import get
 import BeautifulSoup as bs
+from flask import Flask
 
+from models import Meme, db, setup
+
+app = Flask(__name__)
+db.init_app(app)
+
+
+conf = json.load(open('config.json', 'rb'))['develop']
+app.config.update(conf)
 
 def visible(element):
     if element.parent.name in ['style', 'script', '[document]', 'head',
@@ -13,6 +24,16 @@ def visible(element):
     if len(element) < 1 or element.isspace():
         return False
     return True
+
+def get_uri(meme_id):
+    with app.test_request_context():
+        memes = Meme.query.filter(Meme.id == int(meme_id)).all()
+        if not memes:
+            raise ValueError('meme: %s not found' % meme_id)
+        if memes[0].uri:
+            return memes[0].uri
+        else:
+            return None
 
 def get_data(uri):
     resp = get(uri)
@@ -25,6 +46,25 @@ def get_data(uri):
     else:
         title = 'unknown'
     return status_code, title, ' '.join(page_text)
+
+
+def scrape_page(meme_id):
+    uri = get_uri(meme_id)
+    if not uri:
+        return None
+    code, title, data = get_data(uri)
+    try:
+        setup('develop')
+        with app.test_request_context():
+            ngt = Meme.query.filter(Meme.id == int(meme_id))
+            now = datetime.datetime.now()
+            ngt.update({'checked': now, 'status_code': code, 'content': data})
+            ngt[0].update()
+            db.session.commit()
+    except ValueError as err:
+        db.session.rollback()
+        print err.message
+
 
 
 if __name__ == "__main__":
