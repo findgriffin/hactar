448ec186ff7fd848d31c82f139fd8b5c7b230f3f
David Griffin <findgriffin@gmail.com>
2013-09-27 16:09:11 +1000

    some changes to run.py, particularly around db creation

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 import logging
+import os
+import sys
 from logging.handlers import RotatingFileHandler
 from json import load
 
@@ -9,21 +11,37 @@ from tornado.ioloop import IOLoop
 
 from app import app
 
+# apparently importing whooshalchemy and Meme is required to make whooshalchemy
+# work, this seems like too much magic
+import flask.ext.whooshalchemy
+from models import Meme, db
 
-def main():
+
+def main(test=False):
     """Start tornado running hactar."""
-    config = load(open('config.json', 'rb'))
-    app.config.update(config)
+    conf = load(open('config.json', 'rb'))
+    if not test:
+        app.config.update(conf)
+    
 
-    handler = RotatingFileHandler(config['LOG_LOCATION'], maxBytes=100000,
+    logpath = os.path.join(conf['LOG_DIR'], conf['LOG_MAIN'])
+    handler = RotatingFileHandler(logpath, maxBytes=100000,
             backupCount=4)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
 
+    with app.app_context():
+        db.init_app(app)
+    if not os.path.exists(conf['SQLALCHEMY_DATABASE_URI'].lstrip('sqlite:///')):
+        with app.test_request_context():
+            db.create_all()
+    app.logger.debug('starting app with config: %s' % app.config)
     http_server = HTTPServer(WSGIContainer(app))
     http_server.listen(8080)
     IOLoop.instance().start()
 
 
 if __name__ == "__main__":
+    if sys.argv[-1] == 'test':
+        main(True)
     main()
