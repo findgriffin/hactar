45e1608c5d79c5b62133214f3c9f417289160e73
David Griffin <findgriffin@gmail.com>
2013-09-28 00:54:01 +1000

    move all meme related views to hactar/memes.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ db.init_app(app)
 app.config.from_envvar('HACTAR_SETTINGS', silent=True)
 with app.app_context():
     import errors
+    import hactar.memes
 
 @app.teardown_appcontext
 def close_db(error):
@@ -31,98 +32,6 @@ def close_db(error):
 
 @app.route('/')
 def home():
-    return redirect(url_for('memes'))
-
-@app.route('/memes', methods=['GET', 'POST'])
-def memes():
-    """This is actually kind of the home page."""
-    if request.method == 'POST':
-        if not session.get('logged_in'):
-            abort(401)
-        uri = unicode(request.form['uri'])
-        text = unicode(request.form['desc'])
-        try:
-            ngt = Meme(text=text, uri=uri)
-            db.session.add(ngt)
-            db.session.commit()
-            flash('New meme was successfully added')
-        except ValueError as err:
-            app.logger.error('got error: %s' % err.message)
-            db.session.rollback()
-            flash(err.message)
-        except IntegrityError as err:
-            db.session.rollback()
-            if 'primary key must be unique' in err.message.lower():
-                app.logger.debug("can't add duplicate meme: %s" % uri )
-                flash('Meme with that URI or description already exists')
-            else:
-                abort(500)
-    else:
-        terms = request.args.get('q')
-        if terms:
-            app.logger.debug('looking for memes with terms: %s' % terms)
-            filtered = Meme.query.whoosh_search(terms)
-            memes = filtered.order_by(Meme.modified.desc())
-            return render_template('memes.html', memes=memes,
-                    add=False)
-    memes = Meme.query.order_by(Meme.modified.desc())
-    return render_template('memes.html', memes=memes, add=True)
-
-@app.route('/memes/<int:meme>', methods=['GET', 'POST'])
-def meme_handler(meme):
-    if request.method == 'GET':
-        return get_meme(meme)
-    elif 'delete' in request.form and request.form['delete'] == 'Delete':
-        return delete_meme(meme)
-    else:
-        return update_meme(meme)
-
-
-def get_meme(meme):
-    """Edit (or delete) a meme."""
-    memes = Meme.query.filter(Meme.id == int(meme)).all()
-    if not memes:
-        abort(404)
-    return render_template('meme.html', meme=memes[0])
-
-def update_meme(meme):
-    """Update a meme (i.e. implement an edit to a meme)"""
-    try:
-        int(meme)
-    except ValueError:
-        abort(400)
-    if not session.get('logged_in'):
-        abort(401)
-    app.logger.debug('updating meme: %s' % meme)
-    text = unicode(request.form['text'])
-    try:
-        ngt = Meme.query.filter(Meme.id == int(meme))
-        ngt.update({'text': text})
-        ngt[0].update()
-        db.session.commit()
-        flash('Meme successfully modified')
-    except ValueError as err:
-        db.session.rollback()
-        flash(err.message)
-    return redirect(url_for('memes'))
-
-def delete_meme(meme):
-    """Remove a meme from the db."""
-    if not session.get('logged_in'):
-        abort(401)
-    try:
-        Meme.query.filter(Meme.id == int(meme)).delete()
-        db.session.commit()
-        flash('Meme successfully deleted')
-    except ValueError as err:
-        db.session.rollback()
-        flash(err.message)
-    except IntegrityError as err:
-        db.session.rollback()
-        if 'primary key must be unique' in err.message.lower():
-            flash('Meme with that URI or description already exists')
-        else:
-            abort(500)
     return redirect(url_for('memes'))
 
 @app.route('/login', methods=['GET', 'POST'])
