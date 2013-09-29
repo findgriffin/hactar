03686d8c8b9d5846f10b0a59a16182c9faab7103
David Griffin <findgriffin@gmail.com>
2013-09-29 11:14:32 +1000

    make scraper.crawl actually work!

--- hactar/scraper.py
+++ hactar/scraper.py
@@ -8,9 +8,11 @@ import BeautifulSoup as bs
 from flask import Flask, current_app
 from celery import Celery
 
-from models import Meme, db, setup
+from models import Meme
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import time
 
-app = current_app
 
 
 conf = json.load(open('config.json', 'rb'))['develop']
@@ -22,27 +24,28 @@ def visible(element):
     if element.parent.name in ['style', 'script', '[document]', 'head',
             'title']:
         return False
-    elif re.match('<!--.*-->', str(element)):
+    elif re.match('<!--.*-->', unicode(element)):
         return False
     if len(element) < 1 or element.isspace():
         return False
     return True
 
-def get_uri(meme_id):
-    with app.test_request_context():
-        memes = Meme.query.filter(Meme.id == int(meme_id)).all()
-        if not memes:
-            raise ValueError('meme: %s not found' % meme_id)
-        if memes[0].uri:
-            return memes[0].uri
-        else:
-            return None
+
+def get_uri(meme_id, sesh):
+    """ Map meme_id to uri (or None), must be given a db session"""
+    memes = sesh.query(Meme).filter(Meme.id == int(meme_id)).all()
+    if not memes:
+        raise ValueError('meme: %s not found' % meme_id)
+    if memes[0].uri:
+        return memes[0].uri
+    else:
+        return None
 
 def get_data(uri):
     resp = get(uri)
     status_code = resp.status_code
     title = re.search('<title>(.*)</title>', resp.content)# just title for now
-    texts = bs.BeautifulSoup(resp.content).findAll(text=True)
+    texts = bs.BeautifulSoup(unicode(resp.content, errors='ignore').encode()).findAll(text=True)
     page_text = filter(visible, texts)
     if title:
         title = title.group().lstrip('<title>').rstrip('</title>')
@@ -50,34 +53,33 @@ def get_data(uri):
         title = 'unknown'
     return status_code, title, ' '.join(page_text)
 
-@celery.task(name='hactar.scaper.crawl')
+@celery.task(name='crawl')
 def crawl(meme_id):
+    db = create_engine(conf['SQLALCHEMY_DATABASE_URI'])
+    Session = sessionmaker(bind=db)
+    sesh = Session()
+
     start = time.time()
-    with app.app_context():
-        db.session()
-        if type(meme_id) == int:
-            uri = get_uri(meme_id)
-            if not uri:
-                return None
-        elif type(meme_id) in (str, unicode):
-            uri = meme_id
-        code, title, data = get_data(uri)
-        try:
-            setup('develop')
-            timeout = conf["BROKER_TRANSPORT_OPTIONS"]["visibility_timeout"]
-            while time.time() < start+timeout:
-                ngt = Meme.query.filter(Meme.id == int(meme_id))
-                if ngt:
-                    break
+    if type(meme_id) == int:
+        uri = get_uri(meme_id, sesh)
+        if not uri:
+            return None
+    elif type(meme_id) in (str, unicode):
+        uri = meme_id
+    status, title, data = get_data(uri)
+    try:
+        timeout = conf["BROKER_TRANSPORT_OPTIONS"]["visibility_timeout"]
+        while time.time() < start+timeout:
             now = datetime.datetime.now()
-            ngt.update({'checked': now, 'status_code': code, 'content': data,
-                'title': title})
-            ngt[0].update()
-            db.session.commit()
-        except ValueError as err:
-            db.session.rollback()
-            print err.message
-    return code, title, data
+            ngt = sesh.query(Meme).filter(Meme.id == int(meme_id)).update({'checked': now, 'status_code': status, 'content': data,
+            'title': title})
+            if ngt:
+                break
+            time.sleep(1)
+        sesh.commit()
+    except ValueError as err:
+        raise err
+    return status, title, data
 
 
 
