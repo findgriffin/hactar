a1bdd76de919557a0f555fb9f59884dcdc34b900
David Griffin <findgriffin@gmail.com>
2013-10-05 23:59:22 +1000

    add 400/bad request error handler

--- hactar/errors.py
+++ hactar/errors.py
@@ -3,6 +3,12 @@ import traceback
 
 from flask import current_app, render_template
 
+@current_app.errorhandler(400)
+def bad_request(exc):
+    """Handle HTTP not found error."""
+    current_app.logger.debug(traceback.format_exc(exc))
+    return render_template('error.html', exc=exc), 404
+
 @current_app.errorhandler(404)
 def not_found(exc):
     """Handle HTTP not found error."""
