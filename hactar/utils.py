118a6dc22467eb04117f36fe705bb10a73207517
David Griffin <dgriffin@toshiba-tap.com>
2013-11-15 12:09:29 +1100

    fix parse_iso8601 and update test to match

--- hactar/utils.py
+++ hactar/utils.py
@@ -13,17 +13,17 @@ def parse_iso8601(text, tz=None):
         raise ValueError('date %s is invalid')
     elif len(parts) == 3:
         first = date(int(parts[0]), int(parts[1]), int(parts[2]))
-        last = first+tdelta(days=1)
+        last = first
     elif len(parts) == 2:
         year = int(parts[0])
         month = int(parts[1])
         first = date(year, month, 1)
         days = calendar.monthrange(year, month)[1]
-        last = first + tdelta(days)
+        last = first + tdelta(days-1)
     elif len(parts) == 1:
         year = int(parts[0])
         first = date(year, 1, 1)
-        last = date(year+1, 1, 1)
+        last = date(year, 12, 31)
     else:
         raise ValueError('too many parts in iso 8601 date')
     first = dtime(first.year, first.month, first.day, 0, 0, 0, 0, tzinfo=tz)
--- test/testutils.py
+++ test/testutils.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from datetime import date
+from datetime import datetime as dtime
 
 from hactar import utils
 
@@ -8,20 +8,25 @@ class TestUtil(TestCase):
 
     def test_iso8601(self):
         func = utils.parse_iso8601
-        self.assertEqual(func('2013'), (date(2013, 1, 1), date(2014, 1, 1)))
-        self.assertEqual(func('1984'), (date(1984, 1, 1), date(1985, 1, 1)))
-        self.assertEqual(func('4052'), (date(4052, 1, 1), date(4053, 1, 1)))
-        self.assertEqual(func('2013-04'), (date(2013, 4, 1), date(2013, 5, 1)))
-        self.assertEqual(func('2013-03'), (date(2013, 3, 1), date(2013, 4, 1)))
-        self.assertEqual(func('2013-02'), (date(2013, 2, 1), date(2013, 3, 1)))
-        self.assertEqual(func('2012-02'), (date(2012, 2, 1), date(2012, 3, 1)))
-        self.assertEqual(func('2000-02'), (date(2000, 2, 1), date(2000, 3, 1)))
-        self.assertEqual(func('2100-02'), (date(2100, 2, 1), date(2100, 3, 1)))
-        self.assertEqual(func('2013-04-8'), (date(2013, 4, 8), date(2013, 4, 9)))
-        self.assertEqual(func('2013-01-31'), (date(2013, 1, 31), date(2013, 2, 1)))
-        self.assertEqual(func('2013-12-31'), (date(2013, 12, 31), date(2014, 1, 1)))
+        self.assertEqual(func('2013'), get_days(2013, 1, 1, 2013, 12, 31))
+        self.assertEqual(func('1984'), get_days(1984, 1, 1, 1984, 12, 31))
+        self.assertEqual(func('4052'), get_days(4052, 1, 1, 4052, 12, 31))
+        self.assertEqual(func('2013-04'), get_days(2013, 4, 1, 2013, 4, 30))
+        self.assertEqual(func('2013-03'), get_days(2013, 3, 1, 2013, 3, 31))
+        self.assertEqual(func('2013-02'), get_days(2013, 2, 1, 2013, 2, 28))
+        self.assertEqual(func('2012-02'), get_days(2012, 2, 1, 2012, 2, 29))
+        self.assertEqual(func('2000-02'), get_days(2000, 2, 1, 2000, 2, 29))
+        self.assertEqual(func('2100-02'), get_days(2100, 2, 1, 2100, 2, 28))
+        self.assertEqual(func('2013-04-8'), get_days(2013, 4, 8, 2013, 4, 8))
+        self.assertEqual(func('2013-01-31'), get_days(2013, 1, 31, 2013, 1, 31))
+        self.assertEqual(func('2013-12-31'), get_days(2013, 12, 31, 2013, 12, 31))
         self.assertRaises(ValueError, func, '2013-02-29')
         self.assertRaises(ValueError, func, '2013-hi-03')
         self.assertRaises(ValueError, func, '2013-13-03')
         self.assertRaises(ValueError, func, 'garbled')
         
+
+def get_days(f_year, f_month, f_day, l_year, l_month, l_day):
+    f_time = dtime(f_year, f_month, f_day, 0, 0)
+    l_time = dtime(l_year, l_month, l_day, 23, 59, 59, 999999)
+    return (f_time, l_time)
